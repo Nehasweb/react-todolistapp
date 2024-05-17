7c0651981ef4329cae4a89424ce4dcc9b0e969ef
Neha Gholap <nehagholap98@gmail.com>
2024-05-17 16:19:05 +0530

    react project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import { TextField, Button, List, ListItem, ListItemText, Typography } from '@mui/material';
 
 function App() {
+
+  const [tasks, setTasks] = useState([]);
+  const [taskText, setTaskText] = useState('');
+
+  const handleInputChange = (event) => {
+    setTaskText(event.target.value);
+  };
+
+  const handleAddTask = () => {
+    if (taskText.trim() !== '') {
+      setTasks([...tasks, taskText]);
+      setTaskText('');
+    }
+  };
+
+  const handleDeleteTask = (index) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+     <Typography variant="h4" gutterBottom>
+        To-Do List
+      </Typography>
+      <div className="task-form">
+        <TextField
+          label="Task"
+          variant="outlined"
+          value={taskText}
+          onChange={handleInputChange}
+          fullWidth
+    
+
+        />
+        <Button variant="contained" onClick={handleAddTask}>
+          Add
+        </Button>
+      </div>
+      <List>
+      {tasks.map((task, index) => (
+          <ListItem key={index}>
+            <ListItemText primary={task} />
+            <Button variant="outlined" onClick={() => handleDeleteTask(index)}>
+              Delete
+            </Button>
+          </ListItem>
+        ))}
+      </List>
     </div>
   );
 }
